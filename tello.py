70e02ab67b09a6f984c4c8b87e9ab2762130c257
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2024-01-07 19:48:22 +0100

    refactor tello, now working, keeping connection alive, start and stop w/ keyboard

--- tello.py
+++ tello.py
@@ -1,36 +1,60 @@
 from djitellopy import Tello
+import keyboard
 import time
 
 
 class TelloController:
     def __init__(self):
         self.tello = Tello()
-        print(f"Batterie: {self.tello.get_battery()}%")
-
-    def connect(self):
         self.tello.connect()
+        self.is_flying = False
 
-    def takeoff(self):
-        self.tello.takeoff()
-        print("Le Tello a décollé")
+    def start(self):
+        battery_level = self.tello.get_battery()
+        if battery_level < 20:
+            print(f"Battery too low for takeoff: {battery_level}%")
+        else:
+            print("Taking off")
+            self.tello.takeoff()
+            self.is_flying = True
 
-    def land(self):
-        self.tello.land()
-        print("Le Tello a atterri")
+    def stop(self):
+        print("Landing")
+        self.tello.send_command_without_return("land")
+        self.is_flying = False
 
-    def emergency_stop(self):
-        self.tello.emergency()
-        print("Arrêt d'urgence activé")
+    def keep_connection_active(self):
+        battery = self.tello.get_battery()
+        print("Battery: " + str(battery))
 
-    def end(self):
-        self.tello.end()
-        print("Connexion terminée")
+    def emergency(self):
+        print("Emergency landing")
+        self.tello.send_command_without_return("emergency")
+        self.is_flying = False
+
+    def run(self):
+        connection_counter = 0
+        try:
+            while True:
+                if keyboard.is_pressed('p') and not self.is_flying:
+                    self.start()
+                elif keyboard.is_pressed('space'):
+                    self.emergency()
+                    break
+
+                if connection_counter >= 50:
+                    self.keep_connection_active()
+                    connection_counter = 0
+                else:
+                    connection_counter += 1
+
+                time.sleep(0.1)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            self.stop()
 
 
-tello = TelloController()
-try:
-    tello.takeoff()
-    time.sleep(5)
-    tello.land()
-finally:
-    tello.end()
+controller = TelloController()
+controller.keep_connection_active()
+controller.run()
