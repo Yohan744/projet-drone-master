5f96652b646ed0b11ed843bf1d0958cefe1ed579
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2024-01-08 22:54:47 +0100

    upadted pinManager and rotator

--- pinManager.py
+++ pinManager.py
@@ -1,12 +1,12 @@
 class PinManager:
     def __init__(self):
         self.pins = {
-            "microphone": 40,
             "joystick_x": 0,
             "joystick_y": 1,
-            "joystick_button": 38,
-            "rotator_clk": 29,
-            "rotator_dt": 31
+            "rotator_dt": 29,
+            "rotator_clk": 31,
+            "joystick_button": 33,
+            "microphone": 35,
         }
 
     def get_pin(self, name):
--- rotator.py
+++ rotator.py
@@ -11,8 +11,8 @@ pin_manager = PinManager()
 ws = create_connection("ws://localhost:8080")
 ws.send(messageManager.create_message(0, "setName", "rotator"))
 
-ws_rover = create_connection("ws://172.20.10.4:8080")
-ws_rover.send(messageManager.create_message(0, "setName", "rotator"))
+# ws_rover = create_connection("ws://172.20.10.4:8080")
+# ws_rover.send(messageManager.create_message(0, "setName", "rotator"))
 
 # Pins du rotocodeur
 CLK = pin_manager.get_pin("rotator_clk")
@@ -36,7 +36,7 @@ try:
                 counter += 1
                 if counter >= 7:
                     ws.send(messageManager.create_message(3, "rotator", "turning"))
-                    ws_rover.send(messageManager.create_message(3, "rotator", "turning"))
+                    # ws_rover.send(messageManager.create_message(3, "rotator", "turning"))
                     counter = 0
             clkLastState = clkState
 
